a4ea402b393b3d500cad4a6c45fc488b4f72d66b
olawwska <olwojciech@gmail.com>
2021-04-06 11:53:16 +0200

    add navigation

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,15 +1,14 @@
 import React from "react";
 
+import NavigationItem from "./NavigationItem";
 import classes from "./NavigationItems.css";
 
 const NavigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <li className={classes.NavigationItem}>
-            <a h ref={props.link} className={props.active ? classes.active : null}>
-                {" "}
-                Burger Builder
-            </a>
-        </li>
+        <NavigationItem link={"/"} active>
+            Burger Builder
+        </NavigationItem>
+        <NavigationItem link={"/"}>Checkout</NavigationItem>
     </ul>
 );
 
