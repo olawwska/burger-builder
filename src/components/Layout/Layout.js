5dd43dcb4314f28681ed0d2ac777de739d47cf0d
olawwska <olwojciech@gmail.com>
2021-04-06 12:57:12 +0200

    add responsive navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,11 +4,13 @@ import classes from "./Layout.css";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import NavigationItems from "../Navigation/NavigationItems/NavigationItems";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) => (
     <Aux>
         <NavigationItems />
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>{props.children}</main>
     </Aux>
 );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,10 @@ import classes from "./Toolbar.css";
 const Toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
-        <nav>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
